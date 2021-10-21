a32d7403f512115abd0ad98353f834f6321a3a2c
Priyanka-Ravichandran-P <priya.r6907@gmail.com>
2021-10-21 22:43:53 +0530

    Firebase Enhancements

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,16 @@
 
 export const environment = {
   production: false,
-  BASE_URL:'https://rawg-video-games-database.p.rapidapi.com'
+  BASE_URL:'https://rawg-video-games-database.p.rapidapi.com',
+  firebase : {
+    apiKey: "AIzaSyDVrMcoOamaVmsmsr3VSBs-2fNeUh0D2LU",
+    authDomain: "videogameapp-15dae.firebaseapp.com",
+    projectId: "videogameapp-15dae",
+    storageBucket: "videogameapp-15dae.appspot.com",
+    messagingSenderId: "88640021943",
+    appId: "1:88640021943:web:91f64cb4fc2d0a71657432",
+    measurementId: "G-T5XXPSHP6X"
+  }
 };
 
 /*
